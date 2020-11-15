f5beb90005947f8e695815e45d492025375c670a
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-11-15 23:57:15 +0900

    vuetifyの適用とフッターの作成

--- sample-app-js/src/components/Footer.vue
+++ sample-app-js/src/components/Footer.vue
@@ -1,9 +1,131 @@
 <template>
-  <footer />
+  <v-footer v-if="signInUser" app class="pa-0">
+    <v-bottom-navigation grow dark>
+      <v-btn class="my-auto" height="56" @click="routerPush('/')">
+        <span>home</span>
+        <v-icon large>home</v-icon>
+      </v-btn>
+      <v-btn class="my-auto" height="56" @click="routerPush('/calendar/day')">
+        <span>daily</span>
+        <v-icon large>view_day</v-icon>
+      </v-btn>
+      <v-btn class="my-auto" height="56" @click="routerPush('/calendar/week')">
+        <span>weekly</span>
+        <v-icon large>view_week</v-icon>
+      </v-btn>
+      <v-btn class="my-auto" height="56" @click="routerPush('/calendar/month')">
+        <span>monthly</span>
+        <v-icon large>event_note</v-icon>
+      </v-btn>
+      <v-menu top open-on-hover offset-y>
+        <template v-slot:activator="{ on }">
+          <div :class="$style.profile" class="py-1 px-1 text-center" v-on="on">
+            <v-avatar :color="signInUser.themeColor">
+              <span class="white-text body-2" v-text="signInUser.nickname" />
+            </v-avatar>
+          </div>
+        </template>
+        <v-card>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in items"
+              :key="index"
+              link
+              @click="handleMethod(item.methodName)"
+            >
+              <v-list-item-icon>
+                <v-icon v-text="item.icon" />
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title" />
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <v-card-actions>
+            <v-btn block @click="signOut">サインアウト</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-menu>
+    </v-bottom-navigation>
+  </v-footer>
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, reactive, toRefs } from '@vue/composition-api';
+import { profileMockData } from '@/store/profile';
 
-export default defineComponent({});
+export default defineComponent({
+  setup(prop, context) {
+    const state = reactive({
+      items: [
+        {
+          title: 'プロフィール',
+          icon: 'account_circle',
+          methodName: 'profile',
+        },
+        {
+          title: '共有する',
+          icon: 'share',
+          methodName: 'share',
+        },
+      ],
+      signInUser: profileMockData,
+    });
+
+    /**
+     * メソッド名を指定してコンポーネントのメソッドを呼び出します。
+     * @param {string} methodName 呼び出すメソッド名
+     */
+    const handleMethod = methodName => {
+      eval(methodName)();
+    };
+
+    /**
+     * 指定したパスに画面遷移を行います。
+     * @param {string} path 遷移先のパス
+     */
+    const routerPush = path => {
+      context.root.$router.push(
+        path,
+        () => {},
+        () => {},
+      );
+    };
+
+    /**
+     * プロフィール画面に遷移します。
+     */
+    const profile = () => {
+      routerPush('/profile');
+    };
+    /**
+     * 共有画面に遷移します。
+     */
+    const share = () => {
+      routerPush('/share');
+    };
+    /**
+     * サインアウトします。
+     */
+    const signOut = () => {
+      routerPush('/sign-in');
+    };
+
+    return {
+      ...toRefs(state),
+      handleMethod,
+      profile,
+      share,
+      signOut,
+      routerPush,
+    };
+  },
+});
 </script>
+
+<style lang="scss" module>
+.profile {
+  cursor: pointer;
+  width: 168px;
+}
+</style>
--- sample-app-js/vue.config.js
+++ sample-app-js/vue.config.js
@@ -1,6 +1,7 @@
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
+  transpileDependencies: ['vuetify'],
   configureWebpack: {
     plugins: [
       // ホットリロード時に stylelint を実行する
