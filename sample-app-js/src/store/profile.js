e3925f048add805ca8cdca3b0efa4fdfe40b7563
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-11-16 05:11:08 +0900

    サインインページの作成

--- sample-app-js/src/store/profile.js
+++ sample-app-js/src/store/profile.js
@@ -1,3 +1,6 @@
+import '@/plugins/composition-api';
+import { reactive } from '@vue/composition-api';
+
 export const profileMockData = {
   userId: '66006b29-727e-4ed8-a3c8-95d4438f66d4',
   userName: 'naminami',
@@ -6,3 +9,5 @@ export const profileMockData = {
   hasAvatar: false,
   mailAddress: 'namihei@example.com',
 };
+
+export const profileStore = reactive({ profile: null });
