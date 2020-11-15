556c23cbb24431ed0b77d05efba8ae9ca3919c44
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-11-15 16:22:20 +0900

    プロジェクトの設定2

--- sample-app-js/src/App.vue
+++ sample-app-js/src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
+    <Footer />
   </div>
 </template>
 
+<script>
+import { defineComponent } from '@vue/composition-api';
+import FooterComponent from '@/components/Footer.vue';
+
+export default defineComponent({
+  components: {
+    Footer: FooterComponent,
+  },
+});
+</script>
+
 <style lang="scss">
 #app {
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
 
   a {
-    font-weight: bold;
     color: #2c3e50;
+    font-weight: bold;
 
     &.router-link-exact-active {
       color: #42b983;
---
+++ sample-app-js/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer />
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({});
+</script>
