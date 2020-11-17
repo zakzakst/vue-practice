dd72710de9a303eb57caa5e82ee067015dcb8c55
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-11-18 02:15:30 +0900

    共有ページの作成

--- sample-app-js/src/views/Share.vue
+++ sample-app-js/src/views/Share.vue
@@ -1,11 +1,72 @@
 <template>
-  <div>
-    <h1>共有</h1>
-  </div>
+  <v-row align="center" justify="center">
+    <v-col cols="12" md="4" class="text-center">
+      <p class="display-1 py-12">共有する</p>
+      <v-card>
+        <v-card-text>
+          <v-text-field
+            v-model="partOfNickname"
+            clearable
+            flat
+            hide-details
+            prepend-inner-icon="search"
+            label="ニックネーム"
+          />
+        </v-card-text>
+        <v-fade-transition group>
+          <v-list-item v-for="(user, index) in filteredUsers" :key="index">
+            <v-list-item-icon>
+              <v-avatar :color="user.themeColor" size="48">
+                <span class="white--text body-2" v-text="user.nickname" />
+              </v-avatar>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="text-left" v-text="user.nickname" />
+              <v-list-item-subtitle class="text-left" v-text="user.userName" />
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn color="primary" @click="share(user.userId)">
+                共有する
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-fade-transition>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api';
+import {
+  defineComponent,
+  reactive,
+  toRefs,
+  computed,
+} from '@vue/composition-api';
+import { searchUsers } from '@/store/shared-user';
 
-export default defineComponent({});
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      partOfNickname: null,
+      filteredUsers: computed(() => {
+        if (!state.partOfNickname) {
+          return null;
+        }
+        return searchUsers(state.partOfNickname);
+      }),
+    });
+    /**
+     * 指定したユーザーにカレンダーを共有します。
+     * @param userId カレンダーを共有するユーザーのID
+     */
+    // eslint-disable-next-line no-unused-vars
+    const share = userId => {};
+
+    return {
+      ...toRefs(state),
+      share,
+    };
+  },
+});
 </script>
